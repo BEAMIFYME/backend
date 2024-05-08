ea309c68f4a02ebb80e7b246e4d1bf2c6bdbb692
NXFinity <matthew.pd.lee@gmail.com>
2024-05-08 18:42:42 +0100

    Update

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,7 +4,16 @@ import { AppService } from './app/app.service';
 import { DatabaseModule } from './database/database.module';
 import { BlogsModule } from './api/blogs/blogs.module';
 import { LoggerService } from '@logger/logger';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import databaseConfig from './config/database.config';
+import securityConfig from './config/security.config';
+import {AuthModule} from "./security/auth/auth.module";
+import { UsersModule } from './api/users/users.module';
+import { PermitModule } from './security/permit/permit.module';
+import {join} from "path";
+import {ServeStaticModule} from "@nestjs/serve-static";
+
 
 
 @Module({
@@ -12,8 +21,25 @@ import { ConfigModule } from "@nestjs/config";
       ConfigModule.forRoot({
         envFilePath: ['apps/api/.env'],
         isGlobal: true,
-        load: [],
+        load: [databaseConfig, securityConfig],
+          validationSchema: Joi.object({
+              // BASIC VALIDATION
+              SERVER_ENV: Joi.string()
+                  .required()
+                  .valid('development', 'production', 'test')
+                  .default('development'),
+              SERVER_DOMAIN: Joi.string().required(),
+              SERVER_PORT: Joi.number().required(),
+              ALLOWED_ORIGINS_URL: Joi.string().required(),
+              ALLOWED_ORIGINS_LOCAL: Joi.string().required(),
+          }),
       }),
+      ServeStaticModule.forRoot({
+          rootPath: join(__dirname, 'client'),
+      }),
+      AuthModule,
+      UsersModule,
+      PermitModule,
       DatabaseModule,
       BlogsModule,
   ],
